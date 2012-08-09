5d40f95e0cdeb48b3f41a2ac1cd9a6acc03765d1
Seth Tisue <seth@tisue.net>
2012-08-09 17:56:02 -0400

    fix notarize.scala to work both on master and 5.0.x branches

--- bin/notarize.scala
+++ bin/notarize.scala
@@ -24,7 +24,14 @@ val requirePreviews = args.nonEmpty && args(0).trim == "1"
 
 // Read version.txt, legal.txt, models directory.
 
-val netlogo = read("resources/system/version.txt").next  // for example "NetLogo 7.8"
+// paper over difference between master and 5.0.x branches
+val path = "resources/system/version.txt"
+val netlogo = read(
+  if (new File(path).exists)
+    path
+  else
+    "headless/" + path).next()  // for example "NetLogo 7.8"
+
 def addSuffix(path: String) =
   path + ".nlogo" + (if(path.startsWith("3D/")) "3d"
                      else "")
