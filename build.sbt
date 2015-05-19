ecd7dce050cf27dea5d9b65a0ffc90fbaecba47c
nicolaspayette <nicolaspayette@gmail.com>
2015-05-19 16:00:21 -0400

    use sbt and travis to run tests on models

---
+++ build.sbt
@@ -0,0 +1,3 @@
+scalaVersion := "2.11.6"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.4" % "test"
---
+++ src/test/scala/org/nlogo/models/Dummy.scala
@@ -0,0 +1,9 @@
+package org.nlogo.models
+
+import org.scalatest.FunSuite
+
+class Dummy extends FunSuite {
+  test("Tests run") {
+    assert(true)
+  }
+}
