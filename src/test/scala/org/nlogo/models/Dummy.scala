f201c7d468e498b734db630cd285a7b79f303e09
nicolaspayette <nicolaspayette@gmail.com>
2015-05-19 16:27:02 -0400

    add comment to dummy test

--- src/test/scala/org/nlogo/models/Dummy.scala
+++ src/test/scala/org/nlogo/models/Dummy.scala
@@ -3,6 +3,7 @@ package org.nlogo.models
 import org.scalatest.FunSuite
 
 class Dummy extends FunSuite {
+  // this is just for testing that Travis runs
   test("Tests run") {
     assert(true)
   }
