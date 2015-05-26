79590dc75539fa9997f309b0ca963e7a76458c26
nicolaspayette <nicolaspayette@gmail.com>
2015-05-26 19:28:01 -0400

    convert spellcheckmodels to scalatest

---
+++ src/test/scala/org/nlogo/models/SpellCheckTests.scala
@@ -0,0 +1,51 @@
+package org.nlogo.models
+
+import java.io.File
+
+import scala.sys.process.ProcessBuilder
+import scala.sys.process.fileToProcess
+import scala.sys.process.stringSeqToProcess
+
+/**
+ * This test is not very sophisticated, but enough to catch the most egregious typos.
+ * False positives are common, so feel free to add liberally to
+ *
+ *     src/test/resources/modelwords.txt
+ *
+ * Control character before words, in particular, cause trouble, e.g.:
+ *
+ *     \nWord
+ *
+ * In those cases, we just add `nWord` to the dictionary.
+ * (https://github.com/NetLogo/NetLogo/issues/302)
+ *
+ * Note that the code is spell checked too. NetLogo keywords and common
+ * variable names are already white listed, but you may need to add some.
+ *
+ * -- NP 2015-05-26
+ */
+class SpellCheckTests extends TestModels {
+
+  val dictPath = new File(getClass.getResource("/modelwords.txt").toURI).getPath
+  val aspell: ProcessBuilder = Seq(
+    "aspell",
+    "--encoding=UTF-8",
+    "--lang=en_US",
+    "--mode=html",
+    "--ignore-case",
+    "--personal", dictPath,
+    "list")
+
+  testModels("Models must not contain typos") {
+    for {
+      model <- _
+      typos = (model.file #> aspell).lineStream
+      if typos.nonEmpty
+      lines = model.content.lines.zipWithIndex.toStream
+      typosDesc = typos.distinct.sorted.map { typo =>
+        val lineNos = lines.filter(_._1 contains typo).map(_._2 + 1)
+        "  " + typo + " (" + lineNos.mkString(", ") + ")"
+      }
+    } yield model.quotedPath + "\n" + typosDesc.mkString("\n")
+  }
+}
