b74721b46ed7e3f5154eebf91b41b9505eea4494
nicolaspayette <nicolaspayette@gmail.com>
2015-05-28 20:55:24 -0400

    modelTests: slight formatting adjustment

--- src/test/scala/org/nlogo/models/Model.scala
+++ src/test/scala/org/nlogo/models/Model.scala
@@ -29,7 +29,7 @@ trait TestModels extends FunSuite {
   def testModels(testName: String)(testFun: Iterable[Model] => Iterable[String]): Unit =
     test(testName) {
       val failures = testFun(Model.models)
-      if (failures.nonEmpty) fail(failures.mkString("\n"))
+      if (failures.nonEmpty) fail(failures.mkString("", "\n", "\n  -- "))
     }
 }
 
