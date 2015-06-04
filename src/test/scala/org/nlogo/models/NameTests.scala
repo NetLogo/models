fe0b9b664c08d3dcfc9b556c4dd5eb36271849bf
nicolaspayette <nicolaspayette@gmail.com>
2015-06-04 18:20:54 -0400

    add test for duplicate model names

---
+++ src/test/scala/org/nlogo/models/NameTests.scala
@@ -0,0 +1,15 @@
+package org.nlogo.models
+
+import org.scalatest.FunSuite
+
+class NameTests extends FunSuite {
+
+  test("There should be no duplicate model names") {
+    val duplicates = Model.models.groupBy(_.name).filter(_._2.size > 1)
+    if (duplicates.nonEmpty) fail(
+      duplicates.map {
+        case (name, models) => name + "\n" + models.map("  " + _.quotedPath).mkString("\n")
+      }.mkString("\n")
+    )
+  }
+}
