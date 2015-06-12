7d1b9154f108e771263e6a22aa1b21dea92bfa0d
nicolaspayette <nicolaspayette@gmail.com>
2015-06-11 22:20:48 -0400

    add test to find deprecated primitives (#68)

---
+++ src/test/scala/org/nlogo/models/CodeTests.scala
@@ -0,0 +1,23 @@
+package org.nlogo.models
+
+class CodeTests extends TestModels {
+
+  testAllModels("Deprecated primitives are not used") { models =>
+
+    type Exemption = String // the base name of an exempted model
+
+    val forbiddenPrimitives: Seq[(String, Seq[Exemption])] = Seq(
+      "__set-line-thickness" -> Seq("Optical Illusions", "Halo Example"),
+      "__clear-all-and-reset-ticks" -> Seq.empty,
+      "__approximate-hsb-old" -> Seq("Fish Spotters HubNet") // until https://github.com/NetLogo/models/issues/70 is fixed
+    )
+
+    for {
+      (prim, exceptions) <- forbiddenPrimitives
+      model <- models
+      if !exceptions.contains(model.baseName)
+      if model.content.contains(prim)
+    } yield model.quotedPath + "\n  uses " + prim
+  }
+
+}
