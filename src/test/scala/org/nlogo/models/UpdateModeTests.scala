c38e276c05827b3af4e18918ffaedcd5dd3d1ca9
nicolaspayette <nicolaspayette@gmail.com>
2015-06-15 19:25:16 -0400

    Sierpinski Simple 3D: use tick-based updates (#58)

--- src/test/scala/org/nlogo/models/UpdateModeTests.scala
+++ src/test/scala/org/nlogo/models/UpdateModeTests.scala
@@ -3,7 +3,6 @@ package org.nlogo.models
 class UpdateModeTests extends TestModels {
   val continuousUpdateModels = Set(
     "./3D/Code Examples/Shapes Example 3D.nlogo3d",
-    "./3D/Sample Models/Sierpinski Simple 3D.nlogo3d",
     "./Code Examples/Ask Ordering Example.nlogo",
     "./Code Examples/Ask-Concurrent Example.nlogo",
     "./Code Examples/Box Drawing Example.nlogo",
