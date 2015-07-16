bb4aab8460ff3bc3e8db65df8be5e0470e1cd498
nicolaspayette <nicolaspayette@gmail.com>
2015-07-16 16:40:42 -0400

    test 3D models have committed images
    ...regardless of what the preview commands say.
    
    If https://github.com/NetLogo/NetLogo/pull/833
    gets completed and merged, this could be reverted.

--- src/test/scala/org/nlogo/models/PreviewImagesTests.scala
+++ src/test/scala/org/nlogo/models/PreviewImagesTests.scala
@@ -6,17 +6,24 @@ import org.apache.commons.io.FileUtils.readFileToString
 
 class PreviewImagesTests extends TestModels {
 
+  testLibraryModels("3D models should have committed images, regardless of what the preview commands say") {
+    for {
+      model <- _
+      if model.is3d && !model.previewFile.isFile
+    } yield "\"" + model.previewFile.getPath + "\" should be committed"
+  }
+
   testLibraryModels("Models requiring manual previews should have committed images") {
     for {
       model <- _
-      if model.needsManualPreview && !model.previewFile.isFile
+      if !model.is3d && model.needsManualPreview && !model.previewFile.isFile
     } yield "\"" + model.previewFile.getPath + "\" should be committed"
   }
 
   testLibraryModels("Only models requiring manual previews should have committed images") {
     for {
       model <- _
-      if !model.needsManualPreview && model.previewFile.isFile
+      if !model.is3d && !model.needsManualPreview && model.previewFile.isFile
     } yield "\"" + model.previewFile.getPath + "\" should not be committed"
   }
 
@@ -24,7 +31,7 @@ class PreviewImagesTests extends TestModels {
     val ignored = readFileToString(new File(".gitignore"), "UTF-8").lines.toSet
     for {
       model <- _
-      if !model.needsManualPreview
+      if !model.is3d && !model.needsManualPreview
       imagePath = model.previewFile.getPath.drop(1)
       if !ignored.contains(imagePath)
     } yield imagePath + " should be added to .gitignore"
