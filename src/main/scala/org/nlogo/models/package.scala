2790cc421dec885346c60b24db320f756686f880
nicolaspayette <nicolaspayette@gmail.com>
2015-08-21 17:25:47 -0400

    move withWorkspace to package object

---
+++ src/main/scala/org/nlogo/models/package.scala
@@ -0,0 +1,17 @@
+package org.nlogo
+
+import org.nlogo.headless.HeadlessWorkspace
+
+package object models {
+  def withWorkspace[A](model: Model)(f: HeadlessWorkspace => A) = {
+    val workspace = HeadlessWorkspace.newInstance
+    try {
+      workspace.compilerTestingMode = true
+      workspace.silent = true
+      // open the model from path instead of content string so that
+      // the current directory gets set (necessary for `__includes`)
+      workspace.open(model.file.getCanonicalPath)
+      f(workspace)
+    } finally workspace.dispose()
+  }
+}
