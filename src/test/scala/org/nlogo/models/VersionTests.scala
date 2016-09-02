94027bd6bd30ac5e6351e259945a749fc5f37fd9
Nicolas Payette <nicolaspayette@gmail.com>
2016-09-02 12:31:48 +0100

    tests: accept/require 6.0-BETA1 version

--- src/test/scala/org/nlogo/models/VersionTests.scala
+++ src/test/scala/org/nlogo/models/VersionTests.scala
@@ -34,7 +34,7 @@ class VersionTests extends TestModels {
   }
 
   val acceptedVersions =
-    Set("6.0-M9", "6.0-RC1")
+    Set("6.0-BETA1")
       .map { "NetLogo " + (if (Version.is3D) "3D " else "") + _ }
   testModels("Version should be one of " + acceptedVersions.mkString(", ")) {
     Option(_).map(_.version.trim).filterNot(acceptedVersions.contains)
