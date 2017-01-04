56fb160943d754a2523c8511aea93dd82800dda0
Connor Bain <connorbain@u.northwestern.edu>
2017-01-04 15:17:51 -0600

    Test Update to accept 6.0

--- src/test/scala/org/nlogo/models/VersionTests.scala
+++ src/test/scala/org/nlogo/models/VersionTests.scala
@@ -34,7 +34,7 @@ class VersionTests extends TestModels {
   }
 
   val acceptedVersions =
-    Set("6.0-BETA2")
+    Set("6.0-BETA2", "6.0")
       .map { "NetLogo " + (if (Version.is3D) "3D " else "") + _ }
   testModels("Version should be one of " + acceptedVersions.mkString(", ")) {
     Option(_).map(_.version.trim).filterNot(acceptedVersions.contains)
