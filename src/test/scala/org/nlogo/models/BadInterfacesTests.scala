5e877dfb7dca707a575153b8b5af86b445d429d9
ERSUCC <isaacbrown5763@gmail.com>
2025-04-04 16:28:46 -0500

    Removed unnecessary test

--- src/test/scala/org/nlogo/models/BadInterfacesTests.scala
+++ src/test/scala/org/nlogo/models/BadInterfacesTests.scala
@@ -15,18 +15,4 @@ class BadInterfacesTests extends TestModels {
       if output.fontSize < 8 || output.fontSize > 14
     } yield s"OUTPUT has font size ${output.fontSize}"
   }
-
-  testModels("Textboxes (i.e., notes) should be tall enough to fit their content") { model =>
-    for {
-      note <- model.widgets.collect { case w: TextBox=> w }
-      text <- note.display.toSeq
-      noteHeight = note.height
-      dummyLabel = new JLabel(s"<html>$text</html>") {
-        setFont(getFont.deriveFont(note.fontSize.toFloat))
-      }
-      desiredHeight = dummyLabel.getPreferredSize.height
-      if noteHeight < desiredHeight
-    } yield s"Content height is $desiredHeight px but note height is $noteHeight px. " +
-            s"It should be $desiredHeight px to properly fit the content:\n$text"
-  }
 }
